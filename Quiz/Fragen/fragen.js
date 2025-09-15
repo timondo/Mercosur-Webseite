67b29eb1f7069c3bb680c0b11163475ac384de5f
timondo <timon@klingel-stade.de>
2025-09-15 14:37:51 +0200

    Hinzufügen von Frage 3 und 4

--- Quiz/Fragen/fragen.js
+++ Quiz/Fragen/fragen.js
@@ -1,13 +1,12 @@
 //JS für das Punkte hochladen
 
-
-
 //1.Frage
 function loadPoints() {
   if (localStorage.getItem("punkte") === null) {
     localStorage.setItem("punkte", 0);
     localStorage.setItem("isdone1", false);
     localStorage.setItem("isdone2", false);
+    localStorage.setItem("isdone3", false);
   } else {
     let Points = localStorage.getItem("punkte");
     document.getElementById("Punkte").innerHTML = "Punkte: " + Points;
@@ -19,7 +18,6 @@ function answer1() {
   if (isdone1) {
     alert("Nicht schummeln, du hast diese Frage schon beantwortet");
     window.location.href = "/Quiz/Fragen/2.html";
-
   } else {
     let aktuellepunkte = parseInt(localStorage.getItem("punkte"), 10) || 0;
     aktuellepunkte += 1;
@@ -27,18 +25,17 @@ function answer1() {
     window.location.href = "/Quiz/Fragen/2.html";
     localStorage.setItem("isdone1", true);
   }
-
 }
 
 function false1() {
-    let isdone1 = localStorage.getItem("isdone1") === "true";
-    if(isdone1) {
-        alert("Nicht schummeln, du hast diese Frage schon beantwortet");
-        window.location.href = "/Quiz/Fragen/2.html"
-    } else{
-        localStorage.setItem("isdone1", true);
-        window.location.href = "/Quiz/Fragen/2.html";
-    }
+  let isdone1 = localStorage.getItem("isdone1") === "true";
+  if (isdone1) {
+    alert("Nicht schummeln, du hast diese Frage schon beantwortet");
+    window.location.href = "/Quiz/Fragen/2.html";
+  } else {
+    localStorage.setItem("isdone1", true);
+    window.location.href = "/Quiz/Fragen/2.html";
+  }
 }
 //2. Frage
 
@@ -46,8 +43,7 @@ function answer2() {
   let isdone2 = localStorage.getItem("isdone2") === "true";
   if (isdone2) {
     alert("Nicht schummeln, du hast diese Frage schon beantwortet");
-    window.location.href = "/Quiz/Fragen/3.html"
-
+    window.location.href = "/Quiz/Fragen/3.html";
   } else {
     let aktuellepunkte = parseInt(localStorage.getItem("punkte"), 10) || 0;
     aktuellepunkte += 1;
@@ -58,16 +54,58 @@ function answer2() {
 }
 
 function false2() {
-    let isdone2 = localStorage.getItem("isdone2") === "true";
-    if(isdone2) {
-        alert("Nicht schummeln, du hast diese Frage schon beantwortet");
-        window.location.href = "/Quiz/Fragen/3.html";
-    } else{
-        localStorage.setItem("isdone2", true);
-        window.location.href = "/Quiz/Fragen/3.html";
-    }
+  let isdone2 = localStorage.getItem("isdone2") === "true";
+  if (isdone2) {
+    alert("Nicht schummeln, du hast diese Frage schon beantwortet");
+    window.location.href = "/Quiz/Fragen/3.html";
+  } else {
+    localStorage.setItem("isdone2", true);
+    window.location.href = "/Quiz/Fragen/3.html";
+  }
 }
 
+function answer3() {
+  let isdone3 = localStorage.getItem("isdone3") === "true";
+  if (isdone3) {
+    alert("Nicht schummeln, du hast diese Frage schon beantwortet");
+    window.location.href = "/Quiz/Fragen/4.html";
+  } else {
+    localStorage.setItem("isdone3", true);
+    window.location.href = "/Quiz/Fragen/4.html";
+  }
+}
 
+function false3() {
+  let isdone3 = localStorage.getItem("isdone3") === "true";
+  if (isdone3) {
+    alert("Nicht schummeln, du hast diese Frage schon beantwortet");
+    window.location.href = "/Quiz/Fragen/4.html";
+  } else {
+    localStorage.setItem("isdone3", true);
+    window.location.href = "/Quiz/Fragen/4.html";
+  }
+}
+
+function anser4() {
+  let isdone4 = localStorage.getItem("isdone4") === "true";
+  if (isdone4) {
+    alert("Nicht schummeln, du hast diese Frage schon beantwortet");
+    window.location.href = "/Quiz/Fragen/5.html";
+  } else{
+    localStorage.setItem("isdone4", true);
+    window.location.href = "/Quiz/Fragen/4.html";
+  }
+}
+
+function false4() {
+  let isdone4 = localStorage.getItem("isdone4") === "true"
+  if(isdone4) {
+        alert("Nicht schummeln, du hast diese Frage schon beantwortet");
+    window.location.href = "/Quiz/Fragen/5.html";
+  } else {
+    localStorage.setItem("isdone4", true);
+    window.location.href = "Quiz/Fragen/5.html";
+  }
+}
 
 window.onload = loadPoints;
